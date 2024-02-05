843cf95b2ebb1514cd78bb83327a387a1e2f27fa
samarth36483 <raipuriyasamarth@gmail.com>
2024-02-05 10:17:53 +0530

    configured multiple user functionality

--- expensetrackerapi/src/main/java/com/samarth/expensetrackerapi/config/WebSecurityConfig.java
+++ expensetrackerapi/src/main/java/com/samarth/expensetrackerapi/config/WebSecurityConfig.java
@@ -1,16 +1,19 @@
 package com.samarth.expensetrackerapi.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpRequest;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Locale;
+
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -21,19 +24,29 @@ public class WebSecurityConfig {
         return http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests((authz) -> authz
-                        .requestMatchers("localhost:8080/api/v1/login/**").permitAll()
+                        .requestMatchers(antMatcher(HttpMethod.POST, "/login")).permitAll()
+                        .requestMatchers(antMatcher(HttpMethod.POST, "/register")).permitAll()
                         .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .build();
     }
 
 //    @Bean
-//    protected void filterChain(AuthenticationManagerBuilder auth) throws Exception{
+//    public SecurityFilterChain filterChain(AuthenticationManagerBuilder auth) throws Exception{
 //        auth.inMemoryAuthentication()
 //                .withUser("samarth").password("12345").authorities("admin")
 //                .and()
-//                .withUser("sam").password("12345").authorities("user")
+//                .withUser("tony").password("12345").authorities("user")
 //                .and()
 //                .passwordEncoder(NoOpPasswordEncoder.getInstance());
 //    }
+    @Autowired
+    void registerProvider(AuthenticationManagerBuilder auth) throws Exception{
+        auth.inMemoryAuthentication()
+                .withUser("samarth").password("12345").authorities("admin")
+                .and()
+                .withUser("tony").password("12345").authorities("user")
+                .and()
+                .passwordEncoder(NoOpPasswordEncoder.getInstance());
+    }
 }
